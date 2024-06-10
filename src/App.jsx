c552dcd35afe9dc6d216150447e1c0b099743926
vandensudarsono <ediabdullah23@gmail.com>
2024-06-11 02:57:14 +0700

    add handing events

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,60 @@
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo"
+import Form from "./components/Form"
+import FilterButton from "./components/FilterButton";
+
 
 function App(props) {
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false};
+    setTask([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTask(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTask = tasks.filter((task) => id !== task.id);
+    setTask(remainingTask);
+  }
+
+  const [tasks, setTask] = useState(props.tasks);
   const taskList = props.tasks?.map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed} 
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
-));
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks": "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-          />
-          <button type="submit" className="btn btn__primary btn__lg">
-            Add
-          </button>
-      </form>
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 task remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
